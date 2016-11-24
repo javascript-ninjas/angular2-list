b150704575d66c4140414655886b750382656e83
Rafał Gierczak <grime85@gmail.com>
2016-11-24 10:49:49 +0100

    Change indent_size to 4 in editor config

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { Angular2ListPage } from './app.po';
 
-describe('angular2-list App', function() {
-  let page: Angular2ListPage;
+describe('angular2-list App', function () {
+    let page: Angular2ListPage;
 
-  beforeEach(() => {
-    page = new Angular2ListPage();
-  });
+    beforeEach(() => {
+        page = new Angular2ListPage();
+    });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
+    it('should display message saying app works', () => {
+        page.navigateTo();
+        expect(page.getParagraphText()).toEqual('app works!');
+    });
 });
